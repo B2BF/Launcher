4c16d9ecd722c3a2e407539d364a33f3d13ae5a1
Matthias Hoste <42743095+matthias-hoste@users.noreply.github.com>
2024-11-22 20:50:58 +0100

    Make sure to create if not exist

--- B2BF.Launcher/Helpers/ProfileHelper.cs
+++ B2BF.Launcher/Helpers/ProfileHelper.cs
@@ -75,10 +75,18 @@ namespace B2BF.Launcher.Helpers
 
                     if (File.Exists(Path.Combine(defaultProfilePath, "Audio.con")) && !File.Exists(Path.Combine(profilePath, "Audio.con")))
                         File.Copy(Path.Combine(defaultProfilePath, "Audio.con"), Path.Combine(profilePath, "Audio.con"));
+                    else if (File.Exists(Path.Combine(defaultProfilePath, "Audio.con")) && !File.Exists(Path.Combine(profilePath, "Audio.con")))
+                        File.Create(Path.Combine(profilePath, "Audio.con"));
+
                     if (File.Exists(Path.Combine(defaultProfilePath, "General.con")) && !File.Exists(Path.Combine(profilePath, "General.con")))
                         File.Copy(Path.Combine(defaultProfilePath, "General.con"), Path.Combine(profilePath, "General.con"));
+                    else if (File.Exists(Path.Combine(defaultProfilePath, "General.con")) && !File.Exists(Path.Combine(profilePath, "General.con")))
+                        File.Create(Path.Combine(profilePath, "General.con"));
+
                     if (File.Exists(Path.Combine(defaultProfilePath, "ServerSettings.con")) && !File.Exists(Path.Combine(profilePath, "ServerSettings.con")))
                         File.Copy(Path.Combine(defaultProfilePath, "ServerSettings.con"), Path.Combine(profilePath, "ServerSettings.con"));
+                    else if (File.Exists(Path.Combine(defaultProfilePath, "ServerSettings.con")) && !File.Exists(Path.Combine(profilePath, "ServerSettings.con")))
+                        File.Create(Path.Combine(profilePath, "ServerSettings.con"));
                 }
                 else
                 {
